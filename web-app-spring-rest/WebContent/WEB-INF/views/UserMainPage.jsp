49a953f2f8bac2240cc394a144149567a35e6639
Eduardo C <edu_ce_1988@hotmail.com>
2017-08-02 18:08:23 +0200

    Create initial MainController which returns to the view created. Create RestController which will be implemented soon to handle CRUD requests. Create the view which just say a text to test that this works. Create the basic configuration one initializer of this.

---
+++ web-app-spring-rest/WebContent/WEB-INF/views/UserMainPage.jsp
@@ -0,0 +1,8 @@
+<html>
+<head>
+	<title>User Main Page</title>
+</head>
+<body>
+	This is to test that controller works
+</body>
+</html>
---
+++ web-app-spring-rest/src/com/eduardo/spring/controller/UsersRestController.java
@@ -0,0 +1,8 @@
+package com.eduardo.spring.controller;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UsersRestController {
+
+}
